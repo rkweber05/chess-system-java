ee7b178901b532f9426c588c46c501abdc868f7c
rkweber05 <rkweber05@outlook.com>
2024-01-22 14:48:52 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
@@ -12,8 +12,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Position position = new Position(3, 5);
-		System.out.println(position);
+		Board board = new Board(8, 8);
 		
 		sc.close();
 
