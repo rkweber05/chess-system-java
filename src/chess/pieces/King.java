d266152acbd0f7e5361d30f994109b5bc3ab8459
rkweber05 <rkweber05@outlook.com>
2024-01-24 17:12:09 -0300

    Possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -14,5 +14,13 @@ public class King extends ChessPiece{ // King = rei
 	public String toString() {
 		return "K";
 	}
+	
+	@Override
+	public boolean[][] possibleMoves(){
+		
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		return mat;
+	}
 
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -15,4 +15,11 @@ public class Rook extends ChessPiece { // Rook = torre
 		return "R";
 	}
 
+	@Override
+	public boolean[][] possibleMoves(){
+		
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		return mat;
+	}
 }
